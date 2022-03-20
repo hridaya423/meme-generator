cf1ad0bb3c9d402b65b1473b3f369ba0b42cddb7
Hridaya Agrawal <hridayahoney@gmail.com>
2022-03-20 11:03:25 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div style={{ textAlign: "center", fontFamily: "Fredoka" }}>
+      <h1>Meme Generator</h1>
+    </div>
+  );
+};
+
+export default Header;
